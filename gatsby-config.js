46793be34ac1f9469d1fddf5dbbea9e84b9acec2
Ethan Muller <ethanbmuller@gmail.com>
2020-04-05 09:21:14 -0400

    update data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ethan Muller's Website`,
+    description: `Hi, I'm Ethan Muller`,
+    author: `@ethanmuller`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
