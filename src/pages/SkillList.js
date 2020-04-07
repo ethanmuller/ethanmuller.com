c36825f134af4a6bd1998202ada3de6ed60e4cf1
Ethan Muller <ethan@heysparkbox.com>
2020-04-06 20:51:48 -0400

    aria label on skills

--- src/pages/SkillList.js
+++ src/pages/SkillList.js
@@ -6,6 +6,7 @@ import shuffle from "../lib/shuffle"
 class SkillList extends React.Component {
   constructor (props) {
     super(props)
+    this.numItems = 27;
     this.state = { links: shuffle(skills) }
   }
 
@@ -25,13 +26,11 @@ class SkillList extends React.Component {
     clearInterval(this.timerID);
   }
 
-
-
   render() {
     return (
-        <ul style={{minWidth: '10rem'}} onClick={this.shuffleList.bind(this)} className="dash-list">
+      <ul style={{minWidth: '10rem'}} aria-label={`a big list of ${this.numItems} items, the items are shuffling every second`} onClick={this.shuffleList.bind(this)} className="dash-list">
         {
-          this.state.links.slice(0,27).map(i => (
+          this.state.links.slice(0,this.numItems).map(i => (
               <li>{i}</li>
           ))
         }
