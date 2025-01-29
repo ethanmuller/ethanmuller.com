dbf537057cc929f347331a0fd1887a54cba5113e
Ethan Muller <ethanbmuller@gmail.com>
2025-01-29 12:16:35 -0500

    add home page with global styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+  h1, footer {
+    font-size: 2rem;
+    margin: 0;
+    font-weight: normal;
+  }
+  h1 {
+    margin-bottom: 1em;
+  }
+  h1 {
+    margin-bottom: 1em;
+  }
+  footer {
+    margin-top: auto;
+  }
+  .major-mono {
+    font-family: "Major Mono Display", serif;
+    font-weight: 400;
+    font-style: normal;
+    text-transform: lowercase;
+  }
+  .plex {
+    font-family: "IBM Plex Mono", serif;
+    font-weight: 300;
+    font-style: normal;
+  }
+  .plex strong {
+    font-weight: 500;
+  }
+  .positive {
+    color: green;
+  }
+  .max {
+    max-width: 33rem;
+  }
+</style>
+
+<svelte:head>
+    <meta property="og:title" content="Ethan Muller's Website">
+    <meta property="og:description" content="Hi, I'm Ethan Muller">
+    <meta name="description" content="Hi, I'm Ethan Muller">
+    <meta property="og:type" content="website">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Ethan Muller's Website">
+    <meta name="twitter:description" content="Hi, I'm Ethan Muller">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;500&family=Major+Mono+Display&display=swap" rel="stylesheet">
+</svelte:head>
+
+<h1 class="major-mono">hi im ethan muller</h1>
+
+<div class="plex max">
+  <p>Welcome to my personal web page.</p>
+
+  <p>I love to use computers to make interactive multimedia.</p>
+
+  <p>I’m interested in computers, phones, free open source software, programming, video games, game dev, sound design, Linux, Mac, Windows, Android, iOS, etc.</p>
+
+  <p>I am currently: <strong class="positive">looking for employment</strong></p>
+
+</div>
+
+<footer class="major-mono">bye im ethan muller</footer>
