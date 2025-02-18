b4e3e2b35f75ea358967bdc8715d4ea335018355
Ethan Muller <ethanbmuller@gmail.com>
2025-02-18 09:51:00 -0500

    update date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -120,7 +120,7 @@
   <footer class="major-mono">
     <div class="pika">
       <img src="/gif/pika.gif" alt="pikachu doing construction" />
-      Last upd<span class="shake">a</span>te: 2025-02-17
+      Last upd<span class="shake">a</span>te: 2025-02-18
     </div>
     <p>bye im ethan muller</p>
   </footer>
